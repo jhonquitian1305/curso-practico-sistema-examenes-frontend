1c47be652f4609dffbeeaadac0e28f2410465a0b
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-08-29 12:01:14 -0500

    feat: form and service to create a user

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  public user = {
+    username: '',
+    password: '',
+    name: '',
+    lastname: '',
+    email: '',
+    phone: ''
+  }
+
+  constructor(
+    private userService: UserService,
+    private snack: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  formSubmit(){
+    console.log(this.user);
+    if(this.user.username == '' || this.user.username == null){
+      this.snack.open("El nombre de usuario es requerido", 'Aceptar', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
+      });
+      return;
+    }
+
+    this.userService.addUser(this.user).subscribe({
+      next: (data) => {
+        console.log(data);
+        alert('Usuario guardado con éxito');
+      },
+      error: (err) => {
+        this.snack.open(`Ha ocurrido un error en el sistema`, 'Aceptar', {
+          duration: 3000,
+          verticalPosition: 'top',
+          horizontalPosition: 'right'
+        });
+      }   
+    });
   }
 
 }
